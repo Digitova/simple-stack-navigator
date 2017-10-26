db7fd10f092d7fb6804a6e0c22481ed64a787be9
Christopher Rathgeb <crathgeb@digitova.com>
2017-10-26 00:47:08 -0400

    exported reducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import * as SimpleStackNavigatorReducer from './reducers/SimpleStackNavigatorReducer'
 
 export {default as SimpleStackNavigator} from "./components/SimpleStackNavigator"
 export { getRoute } from "./library/getRoute"
 export { SimpleStackNavigatorActions } from "./actions/_actions"
 
+
+export { SimpleStackNavigatorReducer }
