32612a949a40d2bf41a3bd5bc994aee3407a9346
Christopher Rathgeb <crathgeb@digitova.com>
2017-11-13 22:56:18 -0500

    removed console.log

--- src/reducers/GoBackPropsReducer.js
+++ src/reducers/GoBackPropsReducer.js
@@ -1,7 +1,6 @@
 import * as types from '../actions/SimpleStackNavigatorActions'
 
 export const simpleStackGoBackProps = (state = {}, action) => {
-	console.log("here!!!!")
 	if (action.type === types.SIMPLE_STACK_SET_GO_BACK_PROPS) {
 		return Object.assign({},state,action.props)
 	} else {
